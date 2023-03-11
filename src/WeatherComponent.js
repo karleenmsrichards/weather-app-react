66a1ff5c862c61313bc55e70c87672f7de618850
Karleen Richards <karleenmsrichards@gmail.com>
2023-03-11 16:30:23 +0000

    made changes to fetch and removed unnecessary useState

--- src/WeatherComponent.js
+++ src/WeatherComponent.js
@@ -7,51 +7,35 @@ import { AstroSun } from "./AstroSun.js";
 import { AstroMoon } from "./AstroMoon.js";
 
 export const WeatherComponent = () => {
-  const [tempCelcius, setTempCelcius] = useState(70);
-  const [isDay, setIsDay] = useState(null);
-  //   const [tempFar, setTempFar] = useState(120);
-  const [weatherDescription, setWeatherDescription] = useState("");
-  const [weatherCode, setWeatherCode] = useState(0);
-  const [placeName, setPlaceName] = useState("");
-  const [country, setCountry] = useState("");
-  const [region, setRegion] = useState("");
-  const [date, setDate] = useState(null);
-  const [chanceOfRain, setChanceOfRain] = useState("");
-  const [humidity, setHumidity] = useState("");
-  const [uv, setUv] = useState("");
-  const [sunrise, setSunrise] = useState(null);
-  const [sunset, setSunset] = useState(null);
-  const [moonrise, SetMoonrise] = useState(null);
-  const [moonset, SetMoonset] = useState(null);
-  ///Current
-  const [realFeelC, setRealFeelC] = useState(0);
-  //   const [realFeelF, setRealFeelF] = useState(0);
+  const [APIResults, setAPIResults] = useState(null);
+  // const tempFar = data?.current?.temp_f;
+  // setTempFar(tempFar);
 
-  const [day1Date, setDay1Date] = useState("");
-  const [day2Date, setDay2Date] = useState("");
-  const [day3Date, setDay3Date] = useState("");
-
-  const [avgTempCDay1, setAvgTempCDay1] = useState(null);
-  const [avgTempCDay2, setAvgTempCDay2] = useState(null);
-  const [avgTempCDay3, setAvgTempCDay3] = useState(null);
-
-  const [day1Icon, setDay1Icon] = useState(null);
-  const [day2Icon, setDay2Icon] = useState(null);
-  const [day3Icon, setDay3Icon] = useState(null);
-
-  //   const [timestamp, setTimestamp] = null;
-  //   const [day1, setDay1] = useState("");
-  //   const [day1Icon, setDay1Icon] = useState(null);
-  //   const [temp1, setTemp1] = useState();
+  // const realfeelF = data?.current?.feelslike_f;
+  // setRealFeelF(realfeelF);
 
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         const long = position.coords.longitude;
         const lat = position.coords.latitude;
+        let timestamp = new Date(position?.timestamp);
+
+        let options = {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        };
+
+        const formattedDate = new Intl.DateTimeFormat("en-us", options).format(
+          new Date(timestamp)
+        );
+
+        console.log(formattedDate);
+        // console.log(options);
 
         fetch(
-          `https://api.weatherapi.com/v1/forecast.json?key=%204c0e921f27e842289ef203706230803&q=${lat},${long}&days=4&aqi=no&alerts=no`
+          `https://api.weatherapi.com/v1/forecast.json?key=%204c0e921f27e842289ef203706230803&q=${lat},${long}&days=6&aqi=no&alerts=no`
         )
           .then((response) => {
             if (!response.ok) {
@@ -60,138 +44,75 @@ export const WeatherComponent = () => {
             return response.json();
           })
           .then((data) => {
-            const tempCelcius = data?.current?.temp_c;
-            setTempCelcius(tempCelcius);
-
-            // const tempFar = data?.current?.temp_f;
-            // setTempFar(tempFar);
-
-            const weatherDescription = data?.current?.condition?.text;
-            setWeatherDescription(weatherDescription);
-
-            const weatherCode = data?.current?.condition?.code;
-            setWeatherCode(weatherCode);
-
-            const chanceOfRain =
-              data?.forecast?.forecastday[0]?.day?.daily_chance_of_rain;
-
-            setChanceOfRain(chanceOfRain);
-
-            const humidity = data?.current?.humidity;
-            setHumidity(humidity);
-
-            const uv = data?.current?.uv;
-            setUv(uv.toFixed(2));
-
-            const date = data?.location?.localtime;
-            setDate(date);
-
-            const placeName = data?.location?.name;
-            setPlaceName(placeName);
-
-            const country = data?.location?.country;
-            setCountry(country);
-
-            const region = data?.location?.region;
-            setRegion(region);
-
-            const isDay = data?.current?.is_day;
-            setIsDay(isDay);
-
-            /////Current Feels like.....
-
-            const realfeelC = data?.current?.feelslike_c;
-            setRealFeelC(realfeelC);
-
-            // const realfeelF = data?.current?.feelslike_f;
-            // setRealFeelF(realfeelF);
-
-            const day1Date = data?.forecast?.forecastday[1]?.date;
-            setDay1Date(day1Date);
-            console.log(day1Date);
-
-            const day2Date = data?.forecast?.forecastday[2]?.date;
-            setDay2Date(day2Date);
-
-            const day3Date = data?.forecast?.forecastday[3]?.date;
-            setDay3Date(day3Date);
-
-            const avgTempCDay1 = data?.forecast?.forecastday[1]?.day.avgtemp_c;
-            setAvgTempCDay1(avgTempCDay1);
-
-            const avgTempCDay2 = data?.forecast?.forecastday[2]?.day.avgtemp_c;
-            setAvgTempCDay2(avgTempCDay2);
-
-            const avgTempCDay3 = data?.forecast?.forecastday[3]?.day.avgtemp_c;
-            setAvgTempCDay3(avgTempCDay3);
-
-            const day1Icon = data?.forecast?.forecastday[1]?.day.condition.code;
-            setDay1Icon(day1Icon);
-
-            const day2Icon = data?.forecast?.forecastday[2]?.day.condition.code;
-            setDay2Icon(day2Icon);
-            console.log(day2Icon);
-
-            const day3Icon = data?.forecast?.forecastday[3]?.day.condition.code;
-            setDay3Icon(day3Icon);
-
-            const sunrise = data?.forecast?.forecastday[0]?.astro.sunrise;
-            setSunrise(sunrise);
-
-            const sunset = data?.forecast?.forecastday[0]?.astro.sunset;
-            setSunset(sunset);
-
-            const moonrise = data?.forecast?.forecastday[0]?.astro.moonrise;
-            SetMoonrise(moonrise);
-
-            const moonset = data?.forecast?.forecastday[0]?.astro.moonset;
-            SetMoonset(moonset);
+            setAPIResults(data);
           })
           .catch((err) => err);
       });
     }
   }, []);
-
   return (
     <div
-      className={isDay === 0 ? "body-night-background" : "body-day-background"}
+      className={
+        APIResults?.current?.is_day === 0
+          ? "body-night-background"
+          : "body-day-background"
+      }
     >
-      <div className={isDay === 0 ? "night-background" : "day-background"}>
+      <div
+        className={
+          APIResults?.current?.is_dayy === 0
+            ? "night-background"
+            : "day-background"
+        }
+      >
         <PrimaryComponent
-          weatherDescription={weatherDescription}
-          tempCelcius={tempCelcius}
-          realFeelC={realFeelC}
-          // tempFar={tempFar}
-          weatherCode={weatherCode}
-          isDay={isDay}
+          weatherDescription={APIResults?.current?.condition?.text}
+          tempCelcius={APIResults?.current?.temp_c}
+          realFeelC={APIResults?.current?.feelslike_c}
+          weatherCode={APIResults?.current?.condition?.code}
+          isDay={APIResults?.current?.is_day}
         />
         <div className="secondary-weather-content-wrapper">
           <SecondaryComponentA
-            placeName={placeName}
-            country={country}
-            region={region}
-            date={date}
+            placeName={APIResults?.location?.name}
+            country={APIResults?.location?.country}
+            region={APIResults?.location?.region}
+            date={APIResults?.location?.localtime}
           />
           <SecondaryComponentB
-            uv={uv}
-            chanceOfRain={chanceOfRain}
-            humidity={humidity}
+            uv={APIResults?.current?.uv.toFixed(2)}
+            chanceOfRain={
+              APIResults?.forecast?.forecastday[0]?.day?.daily_chance_of_rain
+            }
+            humidity={APIResults?.current?.humidity}
           />
         </div>
         <div className="astro-wrapper">
-          <AstroSun sunrise={sunrise} sunset={sunset} />
-          <AstroMoon moonrise={moonrise} moonset={moonset} />
+          <AstroSun
+            sunrise={APIResults?.forecast?.forecastday[0]?.astro.sunrise}
+            sunset={APIResults?.forecast?.forecastday[0]?.astro.sunset}
+          />
+          <AstroMoon
+            moonrise={APIResults?.forecast?.forecastday[0]?.astro.moonrise}
+            moonset={APIResults?.forecast?.forecastday[0]?.astro.moonset}
+          />
         </div>
         <WeatherDays
-          day1Date={day1Date}
-          day2Date={day2Date}
-          day3Date={day3Date}
-          avgTempCDay1={avgTempCDay1}
-          avgTempCDay2={avgTempCDay2}
-          avgTempCDay3={avgTempCDay3}
-          day1Icon={day1Icon}
-          day2Icon={day2Icon}
-          day3Icon={day3Icon}
+          day1Date={APIResults?.forecast?.forecastday[1]?.date}
+          day2Date={APIResults?.forecast?.forecastday[2]?.date}
+          day3Date={APIResults?.forecast?.forecastday[3]?.date}
+          day4Date={APIResults?.forecast?.forecastday[4]?.date}
+          day5Date={APIResults?.forecast?.forecastday[5]?.date}
+          avgTempCDay1={APIResults?.forecast?.forecastday[1]?.day.avgtemp_c}
+          avgTempCDay2={APIResults?.forecast?.forecastday[2]?.day.avgtemp_c}
+          avgTempCDay3={APIResults?.forecast?.forecastday[3]?.day.avgtemp_c}
+          avgTempCDay4={APIResults?.forecast?.forecastday[4]?.day.avgtemp_c}
+          avgTempCDay5={APIResults?.forecast?.forecastday[5]?.day.avgtemp_c}
+          day1Icon={APIResults?.forecast?.forecastday[1]?.day.condition.code}
+          day2Icon={APIResults?.forecast?.forecastday[2]?.day.condition.code}
+          day3Icon={APIResults?.forecast?.forecastday[3]?.day.condition.code}
+          day4Icon={APIResults?.forecast?.forecastday[4]?.day.condition.code}
+          day5Icon={APIResults?.forecast?.forecastday[5]?.day.condition.code}
         />
       </div>
     </div>
--- src/WeatherDays.js
+++ src/WeatherDays.js
@@ -4,20 +4,28 @@ export const WeatherDays = ({
   day1Date,
   day2Date,
   day3Date,
+  day4Date,
+  day5Date,
   avgTempCDay1,
   avgTempCDay2,
   avgTempCDay3,
+  avgTempCDay4,
+  avgTempCDay5,
   day1Icon,
   day2Icon,
   day3Icon,
+  day4Icon,
+  day5Icon,
 }) => {
   let image1 = WeatherConditions.filter((item) => item.code === day1Icon);
   let image2 = WeatherConditions.filter((item) => item.code === day2Icon);
   let image3 = WeatherConditions.filter((item) => item.code === day3Icon);
+  let image4 = WeatherConditions.filter((item) => item.code === day4Icon);
+  let image5 = WeatherConditions.filter((item) => item.code === day5Icon);
 
   return (
     <div className="weather-days-content">
-      <h2>Weather Forecast</h2>
+      <h2>7Day Weather Forecast</h2>
       <div className="weather-days-wrapper weather-days-border-bottom">
         <p className="weather-days-day1-date">{day1Date}</p>
         <img className="weather-days-icon" src={image1[0]?.imageDay} alt="" />
@@ -28,10 +36,20 @@ export const WeatherDays = ({
         <img className="weather-days-icon" src={image2[0]?.imageDay} alt="" />
         <p className="weather-days-temp">{avgTempCDay2}°</p>
       </div>
-      <div className="weather-days-wrapper">
+      <div className="weather-days-wrapper weather-days-border-bottom">
         <p className="weather-days-day">{day3Date}</p>
         <img className="weather-days-icon" src={image3[0]?.imageDay} alt="" />
         <p className="weather-days-temp">{avgTempCDay3}°</p>
+      </div>
+      <div className="weather-days-wrapper weather-days-border-bottom">
+        <p className="weather-days-day">{day4Date}</p>
+        <img className="weather-days-icon" src={image4[0]?.imageDay} alt="" />
+        <p className="weather-days-temp">{avgTempCDay4}°</p>
+      </div>
+      <div className="weather-days-wrapper">
+        <p className="weather-days-day">{day5Date}</p>
+        <img className="weather-days-icon" src={image5[0]?.imageDay} alt="" />
+        <p className="weather-days-temp">{avgTempCDay5}°</p>
       </div>
     </div>
   );
